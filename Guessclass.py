b2fa568f23cbdf2832c59dd4620d30505a7993b3
Hesampr <hessam.pourrostami@gmail.com>
2022-08-14 12:42:50 +0430

    add features

---
+++ Guessclass.py
@@ -0,0 +1,14 @@
+from random import randint as ri
+
+class Guess:
+    number = int()
+    def __init__(self,number1,number2):
+        self.number = ri(number1,number2+1)
+        
+    def CheckTrue(self,n2):
+        if self.number==n2:
+            return True
+        else:
+            return False
+
+
