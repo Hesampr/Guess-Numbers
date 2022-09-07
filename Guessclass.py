9ffdd04ec459d2503c8b6493df9973fe3094748f
Hesampr <hessam.pourrostami@gmail.com>
2022-09-07 14:49:49 +0430

    Some changes on GUessclass

--- Guessclass.py
+++ Guessclass.py
@@ -4,8 +4,9 @@ from random import randint as ri
 #guess number class
 class Guess:
     number = int()
-    def __init__(self,number1,number2):
-        self.number = ri(number1,number2+1)
+    def __init__(self,part):
+        number1=1
+        self.number = ri(number1,part+1)
         
     def CheckTrue(self,n2):
         if self.number==n2:
