1c68edbad85b275d2528621d0ff8db586919bd11
Hesampr <hessam.pourrostami@gmail.com>
2022-09-11 15:45:55 +0430

    Finish project

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk 
-
+import tkinter.messagebox as mb
+from Guessclass import Guess 
 
 Root = tk.Tk()
 Root.title("Guess it")
@@ -8,7 +9,80 @@ Root.geometry("215x300")
 Root.resizable(width=False,height=False)
 
 
+#Definitions
+resstr = tk.StringVar()
+playerstr = tk.StringVar()
+pcstr = tk.StringVar()
 
+#Functions 
+
+def Submitbutton():
+    period = 0
+    lvlint = lvlentry.get()
+    playernumber=guessentry.get()
+
+    if lvlint=="" or playernumber=="" :
+        lvlint = lvlentry.get()
+        playernumber=guessentry.get()
+        resstr.set(" ")
+        playerstr.set(f" ")
+        pcstr.set(f" ")
+     
+        
+    try:
+        
+        lvlint = int(lvlentry.get())
+        playernumber=int(guessentry.get())
+        if lvlint!= 1 and lvlint!= 2 and lvlint!= 3:
+            mb.showerror("Wrong level" , "Please enter correct level")
+            resstr.set(" ")
+            playerstr.set(f" ")
+            pcstr.set(f" ")
+            lvlint = int(lvlentry.get())
+        else:
+
+            if lvlint==1:
+                period = 10
+                if int(guessentry.get()) > 10 or int(guessentry.get()) < 1:
+                    mb.showerror("Period error","Please enter a number between 1-10")
+                    resstr.set(" ")
+                    playerstr.set(f" ")
+                    pcstr.set(f" ")
+                    playernumber=int(guessentry.get())
+            elif lvlint==2:
+                period=100
+                if int(guessentry.get()) > 100 or int(guessentry.get()) < 1:
+                    mb.showerror("Period error","Please enter a number between 1-100")
+                    resstr.set(" ")
+                    playerstr.set(f" ")
+                    pcstr.set(f" ")
+                    playernumber=int(guessentry.get())
+            else:
+                period=1000
+                if int(guessentry.get()) > 1000 or int(guessentry.get()) < 1:
+                    mb.showerror("Period error","Please enter a number between 1-1000")
+                    resstr.set(" ")
+                    playerstr.set(f" ")
+                    pcstr.set(f" ")
+                    playernumber=int(guessentry.get())
+        
+            playobj1 = Guess(period)
+            ans = playobj1.CheckTrue(playernumber)
+        
+
+            if ans:
+                mb.showinfo("Win","You won")
+                resstr.set("Your answer is correct")
+            else:
+                resstr.set("Your answer is wrong")
+            
+            playerstr.set(f"your answer : {playernumber}")
+            pcstr.set(f"Number : {playobj1.number}")
+
+            return ans 
+
+    except:
+        pass
 
 #Entries 
 
@@ -28,19 +102,19 @@ enterlvllabel.place(x=2,y=77)
 enterguesslabel = tk.Label(Root,text="Your guess: ")
 enterguesslabel.place(x=80,y=77)
 
-pc_guess_Label = tk.Label(Root,text="pc")
+pc_guess_Label = tk.Label(Root,textvariable=pcstr)
 pc_guess_Label.place(x=50,y=170)
 
 
-player_guess_Label = tk.Label(Root,text="palyer")
+player_guess_Label = tk.Label(Root,textvariable=playerstr)
 player_guess_Label.place(x=50,y=195)
 
 
-result_Label = tk.Label(Root,text="winner")
+result_Label = tk.Label(Root,textvariable=resstr)
 result_Label.place(x=50,y=220)
 
 #Buttons 
-submit_Button = tk.Button(Root,text="Submit guess")
+submit_Button = tk.Button(Root,text="Submit guess",command=Submitbutton)
 submit_Button.place(x=50,y=140)
 
 
--- Guessclass.py
+++ Guessclass.py
@@ -4,9 +4,9 @@ from random import randint as ri
 #guess number class
 class Guess:
     number = int()
-    def __init__(self,part):
+    def __init__(self,period):
         number1=1
-        self.number = ri(number1,part+1)
+        self.number = ri(number1,period+1)
         
     def CheckTrue(self,n2):
         if self.number==n2:
